5a6f845219459ad55b1228c8b95ad8f5e0ad436e
WuXian <wulileaf@aliyun.com>
2018-01-29 14:33:16 +0800

    Initial commit

---
+++ app/src/main/java/org/zackratos/cs/MainActivity.java
@@ -0,0 +1,17 @@
+package org.zackratos.cs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.btn_test);
+    }
+}
